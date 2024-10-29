036ad9cf3bbe370abc0539778baf196f7e2f152f
Devin Lane <devin.c.lane@gmail.com>
2024-10-29 16:36:35 -0700

    add mongo

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect(process.env.mongoConnectionString, (err, client) => {});
 
 app.listen(3000, () => {
     console.log("listening on port 3000");
 });
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/quotes", (req, res) => {
+    console.log(req.body);
 });
