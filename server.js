387d78ffcb6228dbac9a1856ad915a94e8806a28
Devin Lane <devin.c.lane@gmail.com>
2024-10-30 13:38:13 -0700

    create functionality

--- server.js
+++ server.js
@@ -2,18 +2,28 @@ const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 
-MongoClient.connect(process.env.mongoConnectionString, (err, client) => {});
+const uri = process.env.mongoConnectionString;
 
-app.listen(3000, () => {
-    console.log("listening on port 3000");
-});
+MongoClient.connect(uri)
+    .then((client) => {
+        const db = client.db("star-wars-quotes");
+        const quotesCollection = db.collection("quotes");
 
-app.use(express.urlencoded({ extended: true }));
+        app.listen(3000, () => {
+            console.log("listening on port 3000");
+        });
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
+        app.use(express.urlencoded({ extended: true }));
 
-app.post("/quotes", (req, res) => {
-    console.log(req.body);
-});
+        app.get("/", (req, res) => {
+            res.sendFile(__dirname + "/index.html");
+        });
+
+        app.post("/quotes", (req, res) => {
+            quotesCollection
+                .insertOne(req.body)
+                .then((result) => res.redirect("/"))
+                .catch((error) => console.error(error));
+        });
+    })
+    .catch((error) => console.error(error));
